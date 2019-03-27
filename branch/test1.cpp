f3b68edaad47c3aff07e1cb6866acad9bca3f086
Animesh9 <aniajrip@gmail.com>
2019-03-27 12:29:23 +0530

    committing

---
+++ branch/test1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 5, b = 10, temp;  
+    cout << "Before swapping." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+    temp = a;
+    a = b;
+    b = temp;
---
+++ branch/test2.cpp
@@ -0,0 +1,9 @@
+a = b;
+    b = temp;
+    cout << "\nAfter swapping." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+    return 0;
+}
+
+
+
